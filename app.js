de1eb42e2ba4a1703dcf999e4567e77d7a0818a3
Andrew <aaoyanguren@outlook.com>
2021-01-01 20:38:31 -0800

    Updated and completed app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Variables & Selectors
-
+const labels = document.querySelectorAll('.form-control label');
 
 
 // Event Listeners
@@ -7,3 +7,12 @@
 
 
 // Functions
+labels.forEach(label => {
+  label.innerHTML = label.innerText
+    // turn our string into array of each character
+    .split('')
+    // return array with defined function, add style to span to create delay
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    // convert array back to string
+    .join('')
+})
